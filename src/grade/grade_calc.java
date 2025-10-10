0649d306400fade87d7714bd35615f3fbf3aa941
GS Dash <banitadash007@gmail.com>
2025-10-11 02:29:19 +0530

    task 4th oct.

--- src/grade/grade_calc.java
+++ src/grade/grade_calc.java
@@ -11,13 +11,18 @@ public class grade_calc {
                 int marks=sc.nextInt();
                 char grade=' ';
 
-                if(marks >=90 && marks<=100) grade='A';
-                else if(marks>=80 && marks<=89) grade='B';
-                else if(marks>=70 && marks<=79) grade='C';
-                else if(marks>=60 && marks<=69) grade='D';
-                else if(marks<=59) grade='F';
+                if(marks >=90 && marks<=100)
+                    System.out.println("grade='A' ");
+                else if(marks>=80 && marks<=89)
+                    System.out.println("grade='B' ");
+                else if(marks>=70 && marks<=79)
+                    System.out.println("grade='C' ");
+                else if(marks>=60 && marks<=69)
+                System.out.println("grade='D'");
+                else if(marks<=59)
+                System.out.println("grade='F' ");
+                else
+                System.out.println("plz enter valid mark");
 
-                System.out.println("Grade of the student is "+grade);
-
-                sc.close();
+              //  sc.close();
 }}}
