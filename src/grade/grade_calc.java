cd5da62123a3731a9aaa6d5f5f99e2e4f1fa0654
GS Dash <banitadash007@gmail.com>
2025-10-11 02:18:23 +0530

    task 4th oct.

---
+++ src/grade/grade_calc.java
@@ -0,0 +1,23 @@
+package grade;
+
+import java.util.Scanner;
+
+public class grade_calc {
+    public static void main(String[] args) {
+
+            {
+                Scanner sc= new Scanner(System.in);
+                System.out.println("Enter marks of the student: ");
+                int marks=sc.nextInt();
+                char grade=' ';
+
+                if(marks >=90 && marks<=100) grade='A';
+                else if(marks>=80 && marks<=89) grade='B';
+                else if(marks>=70 && marks<=79) grade='C';
+                else if(marks>=60 && marks<=69) grade='D';
+                else if(marks<=59) grade='F';
+
+                System.out.println("Grade of the student is "+grade);
+
+                sc.close();
+}}}
