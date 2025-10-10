e6d897e2368b4079f59116fbf2ea2fd03004f2b4
GS Dash <banitadash007@gmail.com>
2025-10-11 02:12:30 +0530

    task 4th oct

---
+++ src/fizbuz/chk_fizbuz.java
@@ -0,0 +1,33 @@
+package fizbuz;
+import java.sql.SQLOutput;
+import java.util.Scanner;
+    public class chk_fizbuz {
+        public static void main(String[] args) {
+            int i;
+            Scanner sc=new Scanner(System.in);
+            System.out.println("entr a no between 1 to 100");
+            i=sc.nextInt();
+   // for(i=1;i<=100; i++)
+
+
+                if(i%3==0 && i%5==0) {
+                    System.out.println("fizzbuz");
+
+                } else if (i%5==0) {
+                    System.out.println("buzz");
+                }
+                else if (i%3==0)
+                {
+                    System.out.println("fiz");
+                }
+                else {
+                    System.out.println("print i =" + i);
+
+            }
+        }}
+
+
+
+
+
+
