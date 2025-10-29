b5afe4134063f2f668652a90b85ec0548a4a9b0a
GS Dash <banitadash007@gmail.com>
2025-10-30 00:15:48 +0530

    Task 17

---
+++ src/Task_23/Pallendrome.java
@@ -0,0 +1,26 @@
+package Task_23;
+
+import java.util.Scanner;
+
+public class Pallendrome {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner (System.in);
+        System.out.println("enter a no.");
+        int num=sc.nextInt();
+        int rev=0, temp,rem;
+         temp=num;
+         while(num>0)
+         {
+             rem=num%10;
+             rev=rev*10+rem;
+             num=num/10;
+         }
+        if (temp==rev)
+        {
+            System.out.println( " is pallendrom");
+        }
+        else {
+            System.out.println( " is not pallendrom");
+        }
+    }
+}
